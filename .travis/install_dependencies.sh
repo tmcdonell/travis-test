abaaf9d62a11c76ab72cbbceda4b6ede96b44593
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-07-29 16:53:04 +1000

    --force-reinstalls
    This might break things..?

--- .travis/install_dependencies.sh
+++ .travis/install_dependencies.sh
@@ -5,7 +5,7 @@ set -x
 # Install and upgrade the dependencies. Since we cache the cabal directory, if
 # there are any problems throw it away and try again.
 #
-cabal install --only-dependencies --upgrade-dependencies --enable-tests
+cabal install --only-dependencies --upgrade-dependencies --enable-tests --force-reinstalls
 status=$?
 
 if [ $status -ne 0 ]; then

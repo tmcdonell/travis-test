57dc6dc806a587754ed0272c9d2c034133f75e03
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-03-30 12:23:57 +1100

    gcc-5.0 -> gcc-5

--- .travis/update-ghc-settings.sh
+++ .travis/update-ghc-settings.sh
@@ -7,10 +7,12 @@ function update_ghc_settings {
     BASE=$(stack path --programs 2>/dev/null | tail -n 1)
     VER=$(stack exec ghc -- --numeric-version)
     sed -i'' -e "s,/usr/bin/gcc.*\",/usr/bin/$1\"," ${BASE}/ghc-${VER}/lib/ghc-${VER}/settings
+
+    cat ${BASE}/ghc-${VER}/lib/ghc-${VER}/settings
   fi
 }
 
 update_ghc_settings gcc-4.8
 update_ghc_settings gcc-4.9
-update_ghc_settings gcc-5.0
+update_ghc_settings gcc-5
 

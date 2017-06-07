edff937b86eeaea0144774051550cdecf0fa5dce
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-06-07 21:07:26 +1000

    blork

--- .travis/install-cuda-trusty.sh
+++ .travis/install-cuda-trusty.sh
@@ -6,7 +6,7 @@ set -x  # echo commands before they are executed
 travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo apt-get update -qq
-export CUDA_APT=${CUDA%-*}
+export CUDA_APT=${CUDA:0:3}
 export CUDA_APT=${CUDA_APT/./-}
 travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cublas-dev-${CUDA_APT}
 travis_retry sudo apt-get clean

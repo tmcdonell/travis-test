0d07fc422a3ac9ea9232d27be178dac94434d6ff
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2018-09-24 16:10:06 +0200

    try install cuda-10

--- .travis/install-cuda-trusty.sh
+++ .travis/install-cuda-trusty.sh
@@ -3,7 +3,8 @@
 set -e  # exit immediately on error
 set -x  # echo commands before they are executed
 
-travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+travis_retry sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 
 # travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_${CUDA}_amd64.deb
@@ -11,7 +12,7 @@ travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 # travis_retry sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 
 travis_retry sudo apt-get update -qq
-export CUDA_APT=${CUDA:0:3}
+export CUDA_APT=$(expr ${CUDA} : '\([0-9]*\.[0-9]*\)')
 export CUDA_APT=${CUDA_APT/./-}
 travis_retry sudo apt-get install -y nvidia-settings cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT} cuda-cublas-dev-${CUDA_APT} cuda-cusparse-dev-${CUDA_APT} cuda-cusolver-dev-${CUDA_APT}
 travis_retry sudo apt-get clean
